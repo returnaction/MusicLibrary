988e352e84e462b3c397c778a4866598bab1a270
returnaction <obergannv@gmail.com>
2023-10-18 15:41:40 -0400

    Add Cors

---
+++ MusicLibrary/MusicLibrary/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace MusicLibrary.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+            });
+    }
+}
--- MusicLibrary/MusicLibrary/Program.cs
+++ MusicLibrary/MusicLibrary/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MusicLibrary.Data;
+using MusicLibrary.Extensions;
 using MusicLibrary.Models;
 using MusicLibrary.Repository;
 using MusicLibrary.Repository.IRepository;
@@ -12,6 +13,7 @@ internal class Program
 
         // Add services to the container.
 
+        builder.Services.ConfigureCors();
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -23,6 +25,8 @@ internal class Program
 
 
         var app = builder.Build();
+
+        app.UseCors("CorsPolicy");
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
